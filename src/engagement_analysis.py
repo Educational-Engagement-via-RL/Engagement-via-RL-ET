0a2a392af522af6fb7a3b8995e04eb4517ea2361
zz2979 <zz2979@columbia.edu>
2023-11-28 13:52:41 -0500

    Update engagement_analysis.py

--- src/engagement_analysis.py
+++ src/engagement_analysis.py
@@ -22,10 +22,7 @@ def start_data_reader():
     thread.start()
 
 # Placeholder dimensions for the UI and areas of interest
-UI_WIDTH, UI_HEIGHT = 1021, 768
-AREA_COUNTRY_FLAG = (0, 0, UI_WIDTH // 2, UI_HEIGHT // 2)
-AREA_LOCATION = (UI_WIDTH // 2, 0, UI_WIDTH, UI_HEIGHT // 2)
-AREA_DETAILS_COLORS = (0, UI_HEIGHT // 2, UI_WIDTH, UI_HEIGHT)
+UI_WIDTH, UI_HEIGHT = 1920, 1080
 
 def parse_gaze_data(gaze_data_dic):
     """
@@ -47,41 +44,24 @@ def parse_gaze_data(gaze_data_dic):
 
     return fixation_data
 
-def engagement_area(fixation_data, area, width, height):
-    """
-    Calculate the engagement score for a specific area using the Fixpos2Densemap function.
-    """
-    start_x, start_y, end_x, end_y = area
-    # Filter fixation data for the current area of interest
-    area_data = fixation_data[(fixation_data[:, 0] >= start_x) & (fixation_data[:, 0] <= end_x) &
-                              (fixation_data[:, 1] >= start_y) & (fixation_data[:, 1] <= end_y)]
-
-    # Generate the heatmap for the specific area
-    heatmap = Fixpos2Densemap(area_data, width, height, None)
-
-    # The engagement score might be the sum of the heatmap values or a more complex function
-    engagement_score = np.sum(heatmap)
-
-    return engagement_score
-
-def calculate_engagement(fixation_data):
+def calculate_engagement(fixation_data, width, height):
     """
     Calculate the aggregate engagement level based on fixation data.
     """
     # Normalize and scale the fixation data
     fixation_data -= fixation_data.min(axis=0)
     fixation_data /= fixation_data.max(axis=0)
-    fixation_data[:, 0] *= UI_WIDTH
-    fixation_data[:, 1] *= UI_HEIGHT
+    fixation_data[:, 0] *= width
+    fixation_data[:, 1] *= height
 
-    # Calculate the engagement score for each defined area
-    engagement_country_flag = engagement_area(fixation_data, AREA_COUNTRY_FLAG, UI_WIDTH, UI_HEIGHT)
-    engagement_location = engagement_area(fixation_data, AREA_LOCATION, UI_WIDTH, UI_HEIGHT)
-    engagement_details_colors = engagement_area(fixation_data, AREA_DETAILS_COLORS, UI_WIDTH, UI_HEIGHT)
+    # Generate the heatmap for the entire UI
+    heatmap = Fixpos2Densemap(fixation_data, width, height, None)
 
-    # Aggregate the scores from each area
-    aggregate_engagement = (engagement_country_flag + engagement_location + engagement_details_colors) / 3
-    return aggregate_engagement
+    # The engagement score might be the sum of the heatmap values or a more complex function
+    engagement_score = np.sum(heatmap)
+
+    return engagement_score
+
 
 def get_current_engagement_score():
     """
@@ -92,7 +72,7 @@ def get_current_engagement_score():
         return None  # Return None or some default value if no data is available
 
     fixation_data = parse_gaze_data(data_store)
-    real_time_engagement_score = calculate_engagement(fixation_data)
+    real_time_engagement_score = calculate_engagement(fixation_data, UI_WIDTH, UI_HEIGHT)
     data_store.clear()
 
     return real_time_engagement_score
